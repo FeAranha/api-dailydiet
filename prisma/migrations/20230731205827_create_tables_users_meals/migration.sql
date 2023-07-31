f68c4320b63fd2803f6c3ae2c500ce98cb18ea62
FeAranha <felipearanha.c@gmail.com>
2023-07-31 18:19:04 -0300

    start

---
+++ prisma/migrations/20230731205827_create_tables_users_meals/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "avatar" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "meals" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "mealDateTime" TIMESTAMP(3) NOT NULL,
+    "isDiet" BOOLEAN NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "meals_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "meals" ADD CONSTRAINT "meals_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
