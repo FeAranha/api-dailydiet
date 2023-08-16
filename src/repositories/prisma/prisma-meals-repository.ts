6d3bf21980fa339e4d18a88fd386c9f937336f2b
FeAranha <felipearanha.c@gmail.com>
2023-08-16 16:41:23 -0300

    meals repository

---
+++ src/repositories/prisma/prisma-meals-repository.ts
@@ -0,0 +1,54 @@
+import { Prisma, Meal } from "@prisma/client";
+import { MealsRepository } from "../meals-repository"
+import { prisma } from "@/lib/prisma";
+
+export class PrismaMealsRepository implements MealsRepository {
+  async findAllByUserId(userId: string): Promise<Meal[]> {
+    const meals = await prisma.meal.findMany({
+      where: {
+        userId: userId,
+      },
+    })
+    return meals
+  }
+  async findById(id: string): Promise<Meal | null> {
+    const meal = await prisma.meal.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return meal;
+  }
+  async findOneByUserIdAndMealId(userId: string, mealId: string): Promise<Meal | null> {
+    const meal = await prisma.meal.findFirst({
+      where: {
+        id: mealId,
+        userId: userId,
+      },
+    });
+    return meal;
+  }
+  async create(data: Prisma.MealCreateInput): Promise<Meal> {
+    const meal = await prisma.meal.create({
+      data: data,
+    });
+    return meal;
+  }
+  async delete(id: string) {
+    await prisma.meal.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async update(id: string, data: Prisma.MealUpdateInput): Promise<Meal> {
+    const meal = await prisma.meal.update({
+      where: {
+        id: id,
+      },
+      data: data,
+    });
+    return meal;
+  }
+}
